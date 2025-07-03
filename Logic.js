3c19c74cf75970c688f06480e4751483247409f9
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-03 10:29:01 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,4 +1,15 @@
-const arr = [3,2,1,,5,6,10,20,30,40]
+function fibonacci(num) {
+  if (num === 0) return [0];
+  if (num === 1) return [0, 1];
 
-const exibir = arr.filter((num) => num > 5)
-console.log(exibir);
+  let arr = [0, 1];
+
+  for (let i = 2; i <= num; i++) {      
+    let prox = arr[i - 1] + arr[i - 2];   
+    arr.push(prox);
+  }
+
+  return arr;
+}
+
+console.log(fibonacci(3));
