433af1b7dc14da79d0ca29bfa8d74cf97a734c8a
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-07 11:32:18 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,17 +1,16 @@
-//onst arr = [10, 20, 30]
+//Números Primos
+//Enunciado: Crie uma função que verifique se um número é primo.
 
-function processarNumeros(num, callback){
-  for (let i = 0; i < num.length; i++){
-    callback(num[i])
+function isPrime (num){
+  if (num <= 1){
+    return false
   }
+  for (let i = 2; i < num; i++){
+    if (num % i === 0){
+      return false 
+    }
+  }
+  return true
 }
-
-{/*
-const dobro = num => {
-  console.log(`o dobro de ${num} é : ${num * 2}`);
-}
-*/}
-processarNumeros([10,20,30], (num)=> {
- console.log(`o dobro de ${num} é : ${num * 2}`);
-  
-})
+console.log(`The number 7 is prime? ${isPrime(7)}`)
+console.log(`The number 10 is prime? ${isPrime(10)}`)
