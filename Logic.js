4472167f5e26a7bc116e0586873eb45f35777ed8
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-07 11:11:50 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,15 +1,5 @@
-const arr = [1,2,3,4,5,6,7,8,9]
-
-function processarNumeros (arr, callback){
-  for (let i = 0; i < arr.length; i++){
-    callback(arr[i])
-  }
+function fatorial (num){
+  if (num === 0 || num === 1) return 1
+  return num * fatorial(num - 1)
 }
-
-const exibir = num => {
-  if (num > 5){
-    console.log(`Numero maior que 5: ${num}`);
-  }
-}
-
-processarNumeros(arr, exibir)
+console.log(fatorial(10));
