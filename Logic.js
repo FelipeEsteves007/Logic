68f14b0c9bad618de8dbcf10a432da008f6d6625
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-11 10:35:58 -0300

    add revert

--- Logic.js
+++ Logic.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const arr = [0,1,2,3,4,5,6,7,8,9,10]
 
 function processarNumeros (arr, callback){
@@ -13,3 +14,20 @@ const exibir = num => {
 }
 
 processarNumeros(arr,exibir)
+=======
+const arr = [1,2,3,4,5,6,7,8,9]
+
+function processarNum (arr, callback){
+  for (let i = 0; i < arr.length; i++){
+    callback(arr[i])
+  }
+}
+
+const exibir = num => {
+  if (num > 5) { 
+    console.log(`O ${num} é maior que 5`);
+  }
+}
+
+processarNum(arr,exibir)
+>>>>>>> parent of 0ffea63 (add)
