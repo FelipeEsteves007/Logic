79849bbf9cea8934a49f23e4fef5f3f933d8bf8c
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-11 10:08:00 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,6 +1,15 @@
-function isPalindrome (str){
-    invert = str.split('').reverse().join('')
-    return invert 
+const arr = [0,1,2,3,4,5,6,7,8,9,10]
+
+function processarNumeros (arr, callback){
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i])
+    }
 }
-console.log(isPalindrome('amor'));
-console.log(isPalindrome('roma'));
+
+const exibir = num => {
+    if (num > 5){
+        console.log(`Maior que 5: ${num}`);
+    }
+}
+
+processarNumeros(arr,exibir)
