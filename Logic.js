97f76fb99a1228068f665c9c76d2536450a15ed1
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-12 10:08:49 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-const arr = [0,1,2,3,4,5,6,7,8,9,10]
-
-function processarNumeros (arr, callback){
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i])
-    }
-}
-
-const exibir = num => {
-    if (num > 5){
-        console.log(`Maior que 5: ${num}`);
-    }
-}
-
-processarNumeros(arr,exibir)
-=======
-const arr = [1,2,3,4,5,6,7,8,9]
-
-function processarNum (arr, callback){
-  for (let i = 0; i < arr.length; i++){
-    callback(arr[i])
-  }
-}
-
-const exibir = num => {
-  if (num > 5) { 
-    console.log(`O ${num} é maior que 5`);
-  }
-}
-
-processarNum(arr,exibir)
->>>>>>> parent of 0ffea63 (add)
+const arr = [4,5,6,7,8]
+const filtrarPares = arr => arr.filter(num => num % 2 === 0)
+console.log(filtrarPares(arr));
