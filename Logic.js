bc424af8bf9cb7a1f69377fdfda85a23998655e6
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-15 11:04:46 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,6 +1,17 @@
-function invertString(str) {
-    invert = str.split('').reverse().join('')
-    return invert
-}
-console.log(invertString("amor"));
+function fibonacci (num){
+    if (num === 0){
+        return []
+    }
+    if (num === 1){
+        return [0]
+    }
 
+    const arr = [0,1]
+
+    for (let i = 2; i < num; i++){
+        let prox = arr[i - 1] + arr[i - 2] 
+        arr.push(prox)
+    }
+    return arr 
+}
+console.log(fibonacci(7));
