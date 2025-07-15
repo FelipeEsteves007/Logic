549578f6e72a30183ef22accfbc46e5ba44849b2
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-15 11:27:46 -0300

    tst 2

--- Logic.js
+++ Logic.js
@@ -1,11 +1,14 @@
 const arr = [10, 20, 30]
+
 function processarNumeros (arr, callback){
     for (let i = 0; i < arr.length; i++){
         callback(arr[i])
     }
 }
+
 const dobro = num => {
     console.log(`o dobro de ${num} é: ${num * 2}`);
     
 }
+
 processarNumeros(arr, dobro)
