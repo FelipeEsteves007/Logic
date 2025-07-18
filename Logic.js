0da64d7511edda3c4ece6f1c4d7d109455f46da4
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-18 11:56:50 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,16 +1,27 @@
-const arr = [1,2,3,4,5,7,9]
+{/*Valid Anagram (#242)
+Enunciado:
+Dadas duas strings s e t, retorne true se t for um anagrama de s, e false caso contrário.
 
-function processarNumeros(arr,callback){
-    for(let i = 0; i < arr.length; i++){
-        callback(arr[i])
+Um anagrama é uma palavra ou frase formada reorganizando as letras de outra palavra ou frase, geralmente usando todas as letras originais exatamente uma vez. */}
+
+function isAnagram (s, t){
+    if (s.length !== t.length){
+        return false 
     }
-}
 
-const exibir = num => {
-    if (num > 5){
-        console.log(`${num} é maior que 5`);
-        
+    const count = {}    // contar quantas vezes cada letra aparece na string
+
+    for (let char of s){ //percorre s e add a char 
+        count[char] = (count[char] || 0) + 1 // se ja existe count[char] soma 1 no valor atual, senão começar com zero se soma 1
     }
-}
 
-processarNumeros(arr,exibir)
+    for (let char of t){ //percorre a strinf t 
+        if (!count[char]){ // se não tiver a letra ou tiver alguma letra diferente ou a mais retorna false
+            return false 
+        }
+        count[char]--;
+    }
+    return true 
+}
+console.log(isAnagram("silent","listen"));
+console.log(isAnagram("rat","car"));
