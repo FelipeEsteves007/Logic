ece4c619e8fee0097ed3108734485113a68e76bf
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-07-24 14:09:07 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,21 +1,27 @@
-function validAnagram (s,t){
-    if (s.length !== t.length){
-        return false
-    }
+const precoAcao = [1, 2, 3, 4, 5, 6, 7, 8, 9] // lista de precos 
 
-    const counter = {}
+function lucroMax(precoAcao) { //funcao com parametro na lista 
+    let valorMin = Infinity // valor minimo comeca no mais alto
+    let lucroMax = 0 // o lucro comeca zero pois n temos nada ainda
 
-    for (let char of s){
-        counter[char] = (counter[char] || 0) + 1
-    }
-
-    for (let char of t){
-        if (!counter[char]){
-            return false
+    for (precoAtual of precoAcao){  // percorremos o preco da acao e pra cada preco
+        if (precoAtual < valorMin){ // preco atual for menor do que infinity
+            valorMin = precoAtual // add a valor min, infinity é só o primeiro e vai correndo pela lista e comparando 
+            console.log(`valor min: ${valorMin}`);
         }
-        counter[char]--
+        console.log("----------------------------------");
+        
+        const lucro = precoAtual - valorMin  // pegamos o lucro que é 2-1, 3-1; 4-1...
+        console.log(`lucro: ${lucro}`);
+        console.log("----------------------------------");
+
+        if (lucro > lucroMax){ // se o lucro for mair que o lucro max 
+            console.log(`Lucro max: ${lucroMax}`);
+            lucroMax = lucro  // add a  lucro max 
+        }
+        console.log("FIM DO FOR");
     }
-    return true 
+    console.log("----------------------------------");
+    return lucroMax // retornamos o lucro max 
 }
-console.log(validAnagram("amor", "roma"));
-console.log(validAnagram("amores", "romas"));
+console.log(lucroMax(precoAcao));
