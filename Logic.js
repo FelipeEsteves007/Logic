72a93cfcb3297f70a4b246e46623e93e04bf0d1f
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-07-29 10:03:40 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,23 +1,9 @@
-function validAnagram (s,t){
-    if (s.length !== t.length){
-        return false
+function fatorial (num){
+    if (num === 1 ){
+        return 1
+    } else {
+        return num * fatorial(num - 1)
     }
-
-    const cont = {}
-
-    for (let char of s){
-        cont[char] = (cont[char] || 0) + 1
-    }
-
-    for (let char of t){
-        if (!cont[char]){
-            return false 
-        }
-        cont[char]--
-    }
-    return true 
 }
 
-console.log(validAnagram("ana", "naa"));
-
-console.log(validAnagram("anaa", "nnaa"));
+console.log(fatorial(4));
