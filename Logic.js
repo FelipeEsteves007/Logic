55792c491948d8c8b81851af3eba8f6849924430
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-07-29 10:11:06 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,15 +1,6 @@
-const arr = [1,2,3,4,5,6,7,8,9,9]
+const arr = [1,2,3]
 
-function duplicate (arr){
-    for (let i = 0; i < arr.length; i++){
-        for (let j = i + 1; j < arr.length; j++){
-            if (arr[i] === arr[j]){
-                return true 
-            }
-        }
-    }
-    return false 
-}
+const soma = arr => arr.reduce((soma,atual) => soma + atual,0)
 
-console.log(duplicate(arr));
+console.log(soma(arr));
 
