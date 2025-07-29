033535eaa5d0376ad0e23c478b0d464a61f2a6c3
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-07-29 10:08:32 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,9 +1,15 @@
-function fatorial (num){
-    if (num === 1 ){
-        return 1
-    } else {
-        return num * fatorial(num - 1)
+const arr = [1,2,3,4,5,6,7,8,9,9]
+
+function duplicate (arr){
+    for (let i = 0; i < arr.length; i++){
+        for (let j = i + 1; j < arr.length; j++){
+            if (arr[i] === arr[j]){
+                return true 
+            }
+        }
     }
+    return false 
 }
 
-console.log(fatorial(4));
+console.log(duplicate(arr));
+
