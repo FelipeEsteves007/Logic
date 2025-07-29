53c6ee2f51eee31910e3d2981fa5a1e1522d2ab9
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-07-29 09:58:04 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,9 +1,23 @@
-function rescursivo (n){
-    if (n === 1){
-        return 1
-    } else {
-        return n + rescursivo(n - 1)
+function validAnagram (s,t){
+    if (s.length !== t.length){
+        return false
     }
-}
-console.log(rescursivo(4));
 
+    const cont = {}
+
+    for (let char of s){
+        cont[char] = (cont[char] || 0) + 1
+    }
+
+    for (let char of t){
+        if (!cont[char]){
+            return false 
+        }
+        cont[char]--
+    }
+    return true 
+}
+
+console.log(validAnagram("ana", "naa"));
+
+console.log(validAnagram("anaa", "nnaa"));
