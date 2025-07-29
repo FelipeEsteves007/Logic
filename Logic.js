0b1a253c99443311e0ba40ab473b4077b80c87dc
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-07-29 10:23:23 -0300

    add

--- Logic.js
+++ Logic.js
@@ -1,6 +1,20 @@
-const arr = [1,2,3]
+const precoAcao = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const soma = arr => arr.reduce((soma,atual) => soma + atual,0)
+function lucroMaxAcao(precoAcao) {
+    let precoMin = Infinity
+    let lucroMax = 0
 
-console.log(soma(arr));
+    for (let precoAtual of precoAcao) {
+        if (precoAtual < precoMin) {
+            precoMin = precoAtual
+        }
+        const lucro = precoAtual - precoMin
+        if (lucro > lucroMax) {
+            lucroMax = lucro
+        }
+    }
 
+    return lucroMax
+}
+
+console.log(lucroMaxAcao(precoAcao));
