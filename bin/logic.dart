91410abf1672b731c9db80245ec7ca99f178f1c3
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-04 14:08:55 -0300

    add

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,5 +1,73 @@
-import 'package:logic/logic.dart' as logic;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${logic.calculate()}!');
+void main (){
+  double age = 0;
+  String? country = "";
+
+ // enter age
+  print ("How old are you?");
+  String? inputAge = stdin.readLineSync();
+  
+  if (inputAge != null){
+    if (inputAge!= ""){
+      age = double.parse(inputAge);
+    }
+  }
+
+   print("Where are you lived? (Brazil, EUA, Japan)");
+  String? inputCountry = stdin.readLineSync();
+
+  if (inputCountry != null){
+    if (inputCountry != ""){
+      country = inputCountry;
+    }
+  }
+
+ // functiom country
+  void brazil(){
+    if (age >= 18){
+      print ("You can drive ");
+    } else {
+      print ("You can't drive ");
+    }
+  }
+
+  void eua(){
+    if (age >= 16){
+      print ("You can drive ");
+    } else {
+      print ("You can't drive ");
+    }
+  }
+
+  void japan(){
+    if (age >= 20){
+      print ("You can drive ");
+    } else {
+      print ("You can't drive ");
+    }
+  }
+
+
+  void nation (){
+    switch (country){
+      case "Brazil":
+      brazil();
+      break;
+
+      case "EUA":
+      eua();
+      break;
+
+      case "Japan":
+      japan();
+      break;
+
+      default:
+        print ("Invalid country");
+    }
+  }
+
+  nation();
+
 }
