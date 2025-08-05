c8dc942860dd34686086ee0a2e0b4d990ba23536
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-05 09:53:55 -0300

    add

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,73 +1,8 @@
-import 'dart:io';
-
 void main (){
-  double age = 0;
-  String? country = "";
-
- // enter age
-  print ("How old are you?");
-  String? inputAge = stdin.readLineSync();
-  
-  if (inputAge != null){
-    if (inputAge!= ""){
-      age = double.parse(inputAge);
-    }
+int number = 8;
+  if (number % 2 == 0){
+    print ("This $number is even");
+  } else {
+    print ("This $number is odd");
   }
-
-   print("Where are you lived? (Brazil, EUA, Japan)");
-  String? inputCountry = stdin.readLineSync();
-
-  if (inputCountry != null){
-    if (inputCountry != ""){
-      country = inputCountry;
-    }
-  }
-
- // functiom country
-  void brazil(){
-    if (age >= 18){
-      print ("You can drive ");
-    } else {
-      print ("You can't drive ");
-    }
-  }
-
-  void eua(){
-    if (age >= 16){
-      print ("You can drive ");
-    } else {
-      print ("You can't drive ");
-    }
-  }
-
-  void japan(){
-    if (age >= 20){
-      print ("You can drive ");
-    } else {
-      print ("You can't drive ");
-    }
-  }
-
-
-  void nation (){
-    switch (country){
-      case "Brazil":
-      brazil();
-      break;
-
-      case "EUA":
-      eua();
-      break;
-
-      case "Japan":
-      japan();
-      break;
-
-      default:
-        print ("Invalid country");
-    }
-  }
-
-  nation();
-
 }
