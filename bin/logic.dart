65bd0635052e6723a46c6cb3f1934b5623e8eabe
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-13 14:45:46 -0300

    poo dart

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,21 +1,46 @@
-void main (){
-    int num = 8;
-    bool isPrime = true;
+void main() {
+  Ingrediente banana = Fruta("banana", "Fruta");
+  Ingrediente cenoura = Legume("cenoura", "legume");
+  Ingrediente sal = Tempero("sal", "tempero");
 
-    if (num < 1){
-        isPrime = false;
-    } else {
-        for (var i = 2; i < num; i++){
-            if (num % i == 0){
-                isPrime = false;
-                break;
-            }
-        }
-    }
+  banana.detalhes();
+  cenoura.detalhes();
+  sal.detalhes();
+}
 
-    if(isPrime){
-        print("is Prime");
-    } else {
-        print ("Is not prime");
-    }
+class Ingrediente {
+  String nome;
+  String tipo;
+
+  Ingrediente(this.nome, this.tipo);
+
+  void detalhes() {
+    print("Nome: $nome, Tipo: $tipo");
+  }
+}
+
+class Fruta extends Ingrediente {
+  Fruta(super.nome, super.tipo);
+
+  @override
+  void detalhes() {
+    print("As frutas geralmente não são cozidas nas receitas. ");
+  }
+}
+
+class Legume extends Ingrediente {
+  Legume(super.nome, super.tipo);
+  @override
+  void detalhes() {
+    print("Os legumes geralmente precisam ser cozidos nas receitas.");
+  }
+}
+
+class Tempero extends Ingrediente {
+  Tempero(super.nome, super.tipo);
+
+  @override
+  void detalhes() {
+    print("Os temperos são usados para condimentar os alimentos.");
+  }
 }
