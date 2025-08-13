9fb8e247f822dcc790b746567390c740f368b51c
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-13 08:24:29 -0300

    add dart

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,7 +1,21 @@
-String inverterString (String texto){
-  return texto.split('').reversed.join();
-}
-
 void main (){
-  print(inverterString("amor"));
+    int num = 8;
+    bool isPrime = true;
+
+    if (num < 1){
+        isPrime = false;
+    } else {
+        for (var i = 2; i < num; i++){
+            if (num % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if(isPrime){
+        print("is Prime");
+    } else {
+        print ("Is not prime");
+    }
 }
