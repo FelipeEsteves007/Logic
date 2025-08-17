db9cc2bf8b031c4c58fce2b7e97d4825dbc1042c
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-17 11:19:30 -0300

    add js

--- Logic.js
+++ Logic.js
@@ -1,13 +1,3 @@
-const nums = [1,2,3,4,5,5]
-
-function containsDuplicate(nums){
-    for (let i = 0; i < nums.length; i++){
-        for (let j = i + 1; j < nums.length; j++){
-            if (nums[i] === nums[j]){
-                return true
-            }
-        }
-    }
-    return false 
-}
-console.log(containsDuplicate(nums));
+const arr = [0,1,2,3,4,5,6]
+const filtrarPares = arr => arr.filter(num => num % 2 === 0)
+console.log(filtrarPares(arr));
