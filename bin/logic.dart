fa52dd60188fc7f504cf822bb0cdf626d93a9733
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-19 08:41:42 -0300

    add dart

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,22 +1,23 @@
-// 0 1 1 2 3 5 8
-List<int> fibonacci (int n){ // uma funcao que retorna uma lista com um int como parametro
-  List<int> seq = [];  // declaramos uma lista
+// 0 1 2 3 5 8
 
+List<int> fibonacci(int num) {
+  List<int> seq = [];
 
-  if(n <= 0) return seq; // if a funcao for menor que zero, retorna o array vazio
-  if(n >= 1) seq.add(0); // if a funcao for maior ou igual a um recebe o primeiro elemento 0
-  if(n >= 2) seq.add(1); // if a funcao for maior ou igual dois recebe o segundo elemento 1
-
-  for (int i = 2; i < n; i ++){ // loop começa no dois 
-    int next = seq[i -1] + seq[i - 2];  // next recebe 2 - 1 + 2 - 2 = 1 o terceiro elemento 
-    seq.add(next); // add na lista 
+  if (num <= 0) {
+    return seq;
   }
-  return seq; // retorna o array da funcao 
+
+  if (num >= 1) seq.add(0);
+
+  if (num >= 2) seq.add(1);
+
+  for (int i = 2; i < num; i++) {
+    int next = seq[i - 1] + seq[i - 2];
+    seq.add(next);
+  }
+  return seq;
 }
 
-
-
-
-void main(){
-  print(fibonacci(7)); // chama a funcao
+void main() {
+  print(fibonacci(7));
 }
