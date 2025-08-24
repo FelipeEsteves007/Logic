339e82956c61b797ad4599b11afc5b30d80f8301
unknown <felipeestevesdepaula@gmail.com>
2025-08-24 20:18:00 -0300

    add every

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,9 +1,16 @@
-void main (){
-  List<String> cidades = ["São Paulo", "Curitiba", "Rio de Janeiro", "Brasília", "Fortaleza"];
+bool isStrongPassword (String password){  // funcao boleana onde recebe a senha
+  List<bool Function(String)> rules = [ // lista de funcoes bool
+    (p) => p.length >= 8, // se a senha maior ou igual a 8 
+    (p) => p.contains(RegExp(r'[A-Z]')), // se a senha conter letra maiuscula
+    (p) => p.contains(RegExp(r'[a-z]')),
+    (p) => p.contains(RegExp(r'[0-9]')),
+    (p) => p.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]'))
+  ];
 
-  cidades.sort();
-  print(cidades);
+  return rules.every((rule) => rule(password)); // “Todos os elementos da lista atendem a essa condição?” 
+} 
 
-  cidades.sort((a,b) => b.compareTo(a));
-  print(cidades);
+void main () {
+   print(isStrongPassword("Senha123!"));  // true
+  print(isStrongPassword("fraco"));      // false
 }
