b28434263c39cf25891c97b3ff5d4662cdda190c
unknown <felipeestevesdepaula@gmail.com>
2025-08-25 09:14:58 -0300

    add eevery

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,9 +1,14 @@
+bool isStrongPassword (String password){ 
+  List<bool Function(String)> rules = [
+    (p) => p.length >= 8,
+    (p) => p.contains(RegExp(r'[A-Z]')),
+    (p) => p.contains(RegExp(r'[a-z]')),
+    (p) =>  p.contains(RegExp(r'[0-9]')),
+    (p) =>  p.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]')),
+  ];
+  return rules.every((rule) => rule(password));
+}
 void main (){
-  List<int> idades = [17,19,12,45,70];
-
-  idades.sort();
-  print(idades);
-
-  idades.sort((a, b) => b.compareTo(a));
-  print(idades);
+ print(isStrongPassword("Senha123!"));  
+  print(isStrongPassword("fraco"));
 }
