6da8cfe2e4a7f8140776d9cc0cdc72c322fae1b1
unknown <felipeestevesdepaula@gmail.com>
2025-08-24 21:08:29 -0300

    add doWhile

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,7 +1,9 @@
-void main (){
-  List<String> words = ["Abacaxi", "Banana", "Amora", "Laranja", "Ameixa"];
-
- List<String> withA = words.where((p) => p.startsWith("A")).toList();
- print(withA);
- print(withA[0]);
+void main() {
+  String frase = "Parou! Este código não continua.";
+  int index = 0; // indice de cada espaco ou letra
+  do {
+    print(frase[index]); // printar cada letra (printando ao menos a primeira)
+    index++; // mais mais
+  } while (frase.length >= index && frase[index - 1] != "!"); // enquanto a o tamanho da fra for maior que o numero do index e a posicao de index q ja foi executada
+  // for diferente de ! continua
 }
