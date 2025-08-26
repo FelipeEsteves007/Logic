446a1b4da823d0a9bf220844fd631e1e0df11180
unknown <felipeestevesdepaula@gmail.com>
2025-08-26 09:10:23 -0300

    add sort

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,14 +1,9 @@
-bool isStrongPassword (String password){ 
-  List<bool Function(String)> rules = [
-    (p) => p.length >= 8,
-    (p) => p.contains(RegExp(r'[A-Z]')),
-    (p) => p.contains(RegExp(r'[a-z]')),
-    (p) =>  p.contains(RegExp(r'[0-9]')),
-    (p) =>  p.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]')),
-  ];
-  return rules.every((rule) => rule(password));
-}
 void main (){
- print(isStrongPassword("Senha123!"));  
-  print(isStrongPassword("fraco"));
+  List<String> cidades = ["São Paulo", "Curitiba", "Rio de Janeiro", "Brasília", "Fortaleza"];
+
+  cidades.sort();
+  print(cidades);
+
+  cidades.sort((a, b) => b.compareTo(a));
+  print(cidades);
 }
