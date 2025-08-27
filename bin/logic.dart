c48d7a9e1edd0c15fc5f56ae36d4340ea9a27760
unknown <felipeestevesdepaula@gmail.com>
2025-08-27 09:37:04 -0300

    add every

--- bin/logic.dart
+++ bin/logic.dart
@@ -3,13 +3,13 @@ void main (){
   print(isStrongPassword("fraco"));      // false
 }
 
-bool isStrongPassword (String passwowrd){
-  List< bool Function(String) > rules = [
-    (p) => p.length >= 8,
-    (p) => p.contains(RegExp(r'[A-Z]')),
-    (p) => p.contains(RegExp(r'[a-z]')),
-    (p) => p.contains(RegExp(r'[0-9]')),
-    (p) => p.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]'))
-  ];
-    return rules.every((rule) => rule(passwowrd));
+bool isStrongPassword (String passwowrd){ // chamamos uma funcao bool com a string senha
+  List< bool Function(String) > rules = [ // fazemos uma lista de funcoes bool onde ira receber uma string 
+    (p) => p.length >= 8, // se for maior ou igual a 8
+    (p) => p.contains(RegExp(r'[A-Z]')),// deve conter letra maiuscula
+    (p) => p.contains(RegExp(r'[a-z]')), // deve conter letra miniscula
+    (p) => p.contains(RegExp(r'[0-9]')), // deve conter de 0 a 9
+    (p) => p.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]')) // deve conter caracteres especiais
+  ]; // RegExp é um detector de padroes, o r é de raw string (transforma a string em literal, sem interpretar)
+    return rules.every((rule) => rule(passwowrd)); // every verifica cada funcao da lista de rules, passando a string password e assim verificando se é true ou false
 }
