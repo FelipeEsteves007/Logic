93f64b1173b62c9c37e229345124960bdae20e7d
unknown <felipeestevesdepaula@gmail.com>
2025-08-27 09:18:47 -0300

    add fibonacci

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,8 +1,23 @@
 void main (){
-  List<String> palavras = ["Abacaxi", "Banana", "Amora", "Laranja", "Ameixa"];
+  print(fibonacci(7));
+}
 
-  List<String> withA = palavras.where((caractere) => caractere.startsWith("A")).toList();
-  print(withA);
-  print(withA[0]);
+List<int> fibonacci (n){
+  // 0 1 1 2 3 5 8
 
+  List<int> seq = [];
+
+  if (n < 1 ){
+    return seq;
+  }
+
+  if (n >= 1) seq.add(0);
+
+  if(n >= 2) seq.add(1);
+
+  for (int i = 2; i < n; i++){
+    int next = seq[i-1] + seq[i-2];
+    seq.add(next);
+  }
+  return seq;
 }
