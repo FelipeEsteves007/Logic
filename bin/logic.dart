45fddd5dbd8ddf61e9289e2df44b69116fba6204
unknown <felipeestevesdepaula@gmail.com>
2025-09-01 13:18:33 -0300

    add sublist

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,11 +1,12 @@
 void main (){
-  print(recursiva(7));
+  List<int> num = List.generate(20, (i) => i + 1);
+  print(num);
+
+  List<int> first = num.sublist(0,5);
+  print(first);
+
+  List<int> less = num.sublist(num.length - 5);
+  print(less);
 }
 
-int recursiva (int n){
-  if (n == 1){
-    return 1;
-  }
-  return n + recursiva(n - 1);
-}
 
