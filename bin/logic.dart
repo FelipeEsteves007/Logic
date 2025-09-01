552151e73c00570016755a3421b807bea366fcea
unknown <felipeestevesdepaula@gmail.com>
2025-09-01 13:04:23 -0300

    add recursividade

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,15 +1,11 @@
 void main (){
-  print(isStrongPassword("Senha123!"));  
-  print(isStrongPassword("fraco"));      
+  print(recursiva(7));
 }
 
-bool isStrongPassword (String senha){
-  List<bool Function(String)> validacao = [
-    (senha) => senha.length >= 8,
-    (senha) => senha.contains(RegExp(r'[A-Z]')),
-    (senha) => senha.contains(RegExp(r'[a-z]')),
-    (senha) => senha.contains(RegExp(r'[0-9]')),
-    (senha) => senha.contains(RegExp(r'[!@#\$%^&*(),.?":{}|<>]')),
-  ];
-  return validacao.every((rule) => rule(senha));
+int recursiva (int n){
+  if (n == 1){
+    return 1;
+  }
+  return n + recursiva(n - 1);
 }
+
