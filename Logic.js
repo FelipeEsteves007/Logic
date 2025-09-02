722a995c6e8e3f366ef537f2f29973aa344d14c5
unknown <felipeestevesdepaula@gmail.com>
2025-09-02 08:27:16 -0300

    add js

--- Logic.js
+++ Logic.js
@@ -1,11 +1,21 @@
-function processarNumeros (arr, callback){
-    for (let i = 0; i < arr.length; i++){
-        callback(arr[i]);
+function validAnagram (s, t){
+    if (s.length !== t.length ){ // se o tamanho das duas forem difentes é falso
+        return false
     }
-}
 
-const dobro = num => {
-    console.log(`${num * 2}`);
-}
+    const count = {} // abro um contador
 
-processarNumeros([10, 20, 30], dobro);
+    for (let char of s){ // percorre a string s
+        count[char] =(count[char] || 0) + 1 // // se a letra existir soma 1 se nao começa com 0
+    } // a:1 b:0
+
+    for(let char of t){ // percorre a string t 
+        if (!count[char]){ // se alguma letra for diferente do contador 
+            return false // retorna falso 
+        }
+        count[char]-- // se nao diminui aquela letra
+    }
+    return true // se caso nenhum for falso, validamos como um anagrama
+}
+console.log(validAnagram("silent","listen"));
+console.log(validAnagram("rat","car"));
