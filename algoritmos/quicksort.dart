575db6402ed8102dc3432a093c6944af65bd9a2a
unknown <felipeestevesdepaula@gmail.com>
2025-09-11 13:36:49 -0300

    add quicksort

--- algoritmos/quicksort.dart
+++ algoritmos/quicksort.dart
@@ -1,35 +1,38 @@
-void quickSort (List<int> list, int left, int right){
-  if (left < right){
-    int pi = partition(list, left, right);
+int partition(List<int> arr, int left, int right) {
+  int pivot = arr[right]; // nosso pivo é o ultimo indice
+  int i = left - 1; // comeca apontando pro nada
 
-    quickSort(list, left, pi - 1);
-    quickSort(list, pi + 1, right);
-  }
-}
-
-int partition (list, left, right){
-  int pivot = list[right];
-  int i = left - 1;
-
-  for (int j = left; j < right; j++){
-    if (list[j] <= pivot){
-      i++;
-      int temp = list[i];
-      list[i] = list[j];
-      list[j] = temp;
+  for (int j = left; j < right; j++) {
+    // percorro com o ponteiro j
+    if (arr[j] <= pivot) {
+      // se o indice indicado é menor ou igual ao meu pivo
+      i++; // incrimento meu ponteiro i
+    // troca arr[i] e arr[j]
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
     }
   }
-  int temp = list[i + 1];
-  list[i + 1] = list[right];
-  list[right] = temp;
+  // coloca o pivô na posição certa
+  int temp = arr[i + 1];
+  arr[i + 1] = arr[right];
+  arr[right] = temp;
+
   return i + 1;
 }
 
+void quicksort(List<int> arr, int left, int right) {
+  // arr, seu indice inicial e seu indice final
+  if (left < right) {
+    int pi = partition(arr, left, right); // dividir maiores e menores e achar a posicao certa do pivo
+    quicksort(arr, left, pi - 1);
+    quicksort(arr, pi + 1, right);
+  }
+}
+
 void main (){
-  List<int> numbers = [10, 7, 8, 9, 1, 5];
-  print("Antes da ordenação: $numbers");
+  List<int> arr = [2 ,4, 3, 6, 7, 1, 5];
 
-  quickSort(numbers, 0, numbers.length - 1);
-
-  print("Depois da ordenação: $numbers");
+  quicksort(arr, 0, arr.length - 1);
+  print(arr);
 }
