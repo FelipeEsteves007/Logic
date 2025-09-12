7fc0fd0a41321a34e83907de9ea5279460ef4c49
unknown <felipeestevesdepaula@gmail.com>
2025-09-12 08:29:50 -0300

    add symetricTree

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,27 +1,27 @@
-class Tree { // fazendo uma arvore
-  int val; // seu primeiro valor
-  Tree? right; // filha da direita
-  Tree? left; // filha da esquerda
+class Tree {  // declaramos nossa arvore
+int val;
+Tree? right;
+Tree? left;
 
-  Tree(this.val, [this.left, this.right]); // construtor, esta em [pois pode ser null, nao é obrigatorio]
+Tree(this.val, [this.left, this.right]);
 }
 
-bool sameTree (Tree? p, Tree? q){
-  if (p == null && q == null) return true; // se as duas forem nulas
+bool symmetricTree ( Tree? p, Tree? q){ // passamos seus filhos de cada lado 
+   if (p == null && q == null) return true; // se as duas forem null
+   if (p == null || q == null) return false;// se uma for null
+   if (p.val != q.val) return false; // se o nó tiver valor difente 
 
-  if (p == null || q == null) return false; // se apenas uma for nula
-
-  if(p.val != q.val) return false; // se n tiverem o mesmo valor 
-
-  return sameTree(p.left, q.left) && sameTree(p.right, q.right); // chamandoa as duas recursivamente pra ver se as filhas sao iguais
+  return symmetricTree(p.left, q.right) && symmetricTree(p.right, q.left); // pegamos o lado 
 }
+
+
 
 void main (){
-  Tree p = Tree(1,Tree(2), Tree(3)); 
-  Tree q = Tree(1, Tree(2),Tree(3));
+  Tree root = Tree(1,
+    Tree(2, Tree(3), Tree(4)),
+    Tree(2, Tree(4), Tree(3))
+  );
 
-  Tree p2 = Tree(1);
-
-  print(sameTree(p, q));
-   print(sameTree(p, p2));
+  var result = symmetricTree(root,root);
+  print(result);
 }
