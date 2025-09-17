3b42d23f74c067469e543dfc56e8ab30497c7dff
unknown <felipeestevesdepaula@gmail.com>
2025-09-17 08:30:44 -0300

    add plusOne

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,15 +1,18 @@
-List<int> plusOne(List<int> digits) {
-  for (int i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] < 9) {
-      digits[i]++;
+List<int> plusOne (List<int> digits){
+
+  for (int i = digits.length - 1; i >= 0; i--){
+    if (digits[i] < 9){
+      digits[i] ++;
+      return digits;
     } else {
       digits[i] = 0;
     }
   }
-  return [1, ...digits];
+  return[1, ...digits];
 }
 
-void main() {
+
+void main (){
   print(plusOne([1, 2, 3]));
-  print(plusOne([9, 9, 9]));
+  print(plusOne([9]));
 }
