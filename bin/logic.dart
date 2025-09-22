39b9abe33eee74ad038390486db704c2ab6c639d
unknown <felipeestevesdepaula@gmail.com>
2025-09-22 10:53:52 -0300

    add PlusOne

--- bin/logic.dart
+++ bin/logic.dart
@@ -1,26 +1,18 @@
-class Tree {
-int value;
-Tree? right;
-Tree? left;
-
-Tree(this.value, {this.left, this.right});
+List<int> plusOne (List<int> digits){
+  for (int i = digits.length - 1; i >= 0; i--){
+    if (digits[i] < 9){
+      digits[i] ++;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+  return [1, ...digits];
 }
 
-bool sameTree (Tree? p,Tree? q){
-  if (p == null && q == null) return true;
 
-  if (p == null || q == null) return false;
-
-  if (p.value != q.value) return false;
-
-  return sameTree(p.left, q.left) && sameTree(p.right, q.right);
-}
 
 void main (){
-  var t1 = Tree(1, left: Tree(2), right: Tree(3));
-  var t2 = Tree(1, left: Tree(2), right: Tree(3));
-  var t3 = Tree(1, left: Tree(2));
-
-  print(sameTree(t1, t2));
-  print(sameTree(t1, t3));
+  print(plusOne([1, 2, 3]));
+  print(plusOne([9]));
 }
